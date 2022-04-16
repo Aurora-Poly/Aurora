fe18a375c0486cbbe1a0e73404f8bb2c8ef4e664
LAPTOP-NBI9HOC1\juhyu <ju2136@naver.com>
2022-04-16 14:31:05 +0900

    category, tag추가

--- activity/models.py
+++ activity/models.py
@@ -1,6 +1,28 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+  name = models.CharField(max_length=50)
+  slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+  def __str__(self):
+    return self.name
+  def get_absolute_url(self):
+    return f'/blog/tag/{self.slug}'
+
+
+class Category(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+  slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    verbose_name_plural = 'Categories'
+
 class Act(models.Model):
   title = models.CharField(max_length=30) #제목
   apply = models.CharField(max_length=500) #홈페이지
@@ -12,8 +34,15 @@ class Act(models.Model):
 
   poster_image = models.ImageField(upload_to='activity/images/%Y/%m/%d/', blank=True) #포스터 이미지
 
+  category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+  tag = models.ManyToManyField(Tag, null=True, blank=True)
+  
   def __str__(self):
     return f'[{self.pk}]{self.title}'
 
   def get_absolute_url(self):
     return f'/activity/{self.pk}/'
+
+
+
+  
